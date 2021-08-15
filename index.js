df5a0c606f87ec220aeecfe1be472a8cfc9325ba
Matthew Broadhead <aforesaidbroadhead@gmail.com>
2021-08-15 13:11:17 +0200

    create function stubs and improve readme

---
+++ index.js
@@ -0,0 +1,21 @@
+// https://towardsdatascience.com/how-to-automate-lidar-point-cloud-processing-with-python-a027454a536c
+
+exports.printMsg = function () {
+    console.log('This message is from pointcloud-3d')
+}
+
+exports.reduceSampling = function (data = [], factor = 5) {
+
+}
+
+exports.reduceVoxel = function (data = [], separation = 0.05) {
+
+}
+
+exports.reduceTargetSampling = function (data = [], size = 100) {
+
+}
+
+exports.reduceTargetVoxel = function (data = [], size = 100) {
+
+}
